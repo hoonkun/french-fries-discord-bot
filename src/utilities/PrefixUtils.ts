2e944381934eae6f14921877ea291ea535f6cdc8
gohoon-k <herokun.user@gmail.com>
2021-07-04 15:18:31 +0900

    added used prefix object to return value of hasValidPrefix function

--- src/utilities/PrefixUtils.ts
+++ src/utilities/PrefixUtils.ts
@@ -1,21 +1,29 @@
 import configurations from "../../configurations.json";
 
 import {permutation} from "./ArrayUtils";
-import {Prefix} from "../types";
 
-export function hasValidPrefix(command: string) {
+export interface Prefix {
+    type: string,
+    value: string,
+    suffixEnabled: boolean
+}
+
+export function hasValidPrefix(command: string): [Prefix | null, boolean] {
     let result = false;
+    let usedPrefix: Prefix | null = null;
     permutation(configurations.PREFIXES, configurations.SUFFIXES).forEach((value) => {
         if (result) return;
 
         let prefix: Prefix = value[0];
         let suffix: string = value[1];
 
+        usedPrefix = prefix;
+
         result =
             (command.startsWith(`${prefix.value} `) || command == prefix.value) ||
             (prefix.suffixEnabled && (command.startsWith(`${prefix.value}${suffix} `) || command == `${prefix.value}${suffix}`));
     });
-    return result;
+    return [usedPrefix, result];
 }
 
 export function getPrefixLength(command: string) {
